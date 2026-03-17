777c93954c02c7d7e97193d3d54f3499bc54d978
KuTuGu <wangyufei.kutu@gmail.com>
2026-03-17 18:04:14 +0800

    refactor: langchain -> smolagents

--- src/main.py
+++ src/main.py
@@ -1,61 +1,26 @@
-from langchain_openai import ChatOpenAI
-from langchain.agents import create_agent
-from langchain.agents.middleware import SummarizationMiddleware
-from langchain.agents.middleware import TodoListMiddleware
-from langchain.agents.middleware import ContextEditingMiddleware, ClearToolUsesEdit
-from langchain_community.tools import DuckDuckGoSearchResults
 import gradio as gr
 from langfuse import get_client
-from langfuse.langchain import CallbackHandler
+from smolagents import OpenAIModel, WebSearchTool, CodeAgent, GradioUI
+from openinference.instrumentation.smolagents import SmolagentsInstrumentor
 import os
 from ddgs import DDGS
 
 langfuse = get_client()
-langfuse_handler = CallbackHandler()
+SmolagentsInstrumentor().instrument()
 
-model = ChatOpenAI(
-    model=os.environ["OPENAI_NAME"],
-    max_retries=3,
+model = OpenAIModel(
+    model_id=os.environ["OPENAI_NAME"],
+    api_base=os.environ["OPENAI_URL"],
     api_key=os.environ["OPENAI_API_KEY"],
-    base_url=os.environ["OPENAI_URL"],
-    callbacks=[langfuse_handler],
-)
-agent = create_agent(
-    model=model,
-    tools=[DuckDuckGoSearchResults()],
-    middleware=[
-        SummarizationMiddleware(
-            model=model,
-            trigger=("tokens", 10000),
-            keep=("messages", 20),
-        ),
-        TodoListMiddleware(),
-        ContextEditingMiddleware(
-            edits=[
-                ClearToolUsesEdit(
-                    trigger=100000,
-                    keep=3,
-                ),
-            ],
-        ),
-    ],
-    system_prompt="你是一个乐于助人的个人助手，资料搜索尽量找英语资料，除非是国内网站，交流时请始终以中文回答。",
+    temperature=0.7,
+    max_tokens=10000,
+    top_p=0.9,
 )
 
-def chatbot(msg: str, history: list):
-    full = ""
-    for chunk in agent.stream(
-        {"messages": history + [{"role": "user", "content": msg}]},
-        stream_mode="messages",
-        version="v2",
-    ):
-        if chunk["type"] == "messages":
-            token, metadata = chunk["data"]
-            if metadata['langgraph_node'] == 'model':
-                for block in token.content_blocks:
-                    if block["type"] == "text":
-                        full += block["text"]
-                        yield full
+agent = CodeAgent(
+    model=model, tools=[WebSearchTool()], add_base_tools=True,
+    instructions="请尽量搜索英语资料，但回答时请始终保持中文"
+)
 
-demo = gr.ChatInterface(chatbot)
-demo.launch(server_name="0.0.0.0", server_port=8100)
+gradio_ui = GradioUI(agent, file_upload_folder="uploads")
+gradio_ui.launch(share=False, server_name="0.0.0.0", server_port=8100)
