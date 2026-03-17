53d26133a3aebb8f3aea73abc559d069b67f5834
KuTuGu <wangyufei.kutu@gmail.com>
2026-03-17 20:08:02 +0800

    fix: CatchErrorGradioUI

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 import gradio as gr
 from langfuse import get_client
-from smolagents import OpenAIModel, WebSearchTool, CodeAgent, GradioUI
+from smolagents import OpenAIModel, CodeAgent, GradioUI
+from smolagents import WebSearchTool, WikipediaSearchTool, VisitWebpageTool
+from smolagents import UserInputTool, FinalAnswerTool
 from openinference.instrumentation.smolagents import SmolagentsInstrumentor
 import os
-from ddgs import DDGS
+from typing import Generator
 
 langfuse = get_client()
 SmolagentsInstrumentor().instrument()
@@ -18,9 +20,21 @@ model = OpenAIModel(
 )
 
 agent = CodeAgent(
-    model=model, tools=[WebSearchTool()], add_base_tools=True,
-    instructions="请尽量搜索英语资料，但回答时请始终保持中文"
+    model=model, add_base_tools=True,
+    instructions="请尽量搜索英语资料，但回答时请始终保持中文",
+    tools=[
+        WebSearchTool(), WikipediaSearchTool(), VisitWebpageTool(), FinalAnswerTool()
+    ],
 )
 
-gradio_ui = GradioUI(agent, file_upload_folder="uploads")
+class CatchErrorGradioUI(GradioUI):
+    def _stream_response(self, message: str | dict, history: list[dict]) -> Generator:
+        try:
+            yield from super()._stream_response(message, history)
+        except Exception as e:
+            self.agent.memory.reset()
+            self.agent.monitor.reset()
+            yield str(e)
+
+gradio_ui = CatchErrorGradioUI(agent, file_upload_folder="uploads")
 gradio_ui.launch(share=False, server_name="0.0.0.0", server_port=8100)
